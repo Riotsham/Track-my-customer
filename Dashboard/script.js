6e888ad27c597228cb27569fde06b8f8b2719fc5
abibalasubramaniyan <abirevathi.1312@gmail.com>
2026-02-02 19:29:22 +0530

    Add files via upload

---
+++ Dashboard/script.js
@@ -0,0 +1,8 @@
+let index = 0;
+const slides = document.getElementById("slides");
+const totalSlides = slides.children.length;
+
+setInterval(() => {
+  index = (index + 1) % totalSlides;
+  slides.style.transform = `translateX(-${index * 100}%)`;
+}, 3000);
