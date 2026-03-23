f07ce5e620cb69a435fda5f2a88d72c8118a0520
Riot sam <161737809+Riotsham@users.noreply.github.com>
2026-03-23 11:20:15 +0530

    Add files via upload

---
+++ login page/script.js
@@ -0,0 +1,49 @@
+function login() {
+  let email = document.getElementById("email").value.trim();
+  let password = document.getElementById("password").value.trim();
+
+  let emailError = document.getElementById("emailError");
+  let passError = document.getElementById("passError");
+
+  emailError.style.display = "none";
+  passError.style.display = "none";
+
+  let valid = true;
+
+  if (!email) {
+    emailError.style.display = "block";
+    valid = false;
+  }
+
+  if (!password) {
+    passError.style.display = "block";
+    valid = false;
+  }
+
+  if (!valid) return;
+
+ 
+  let authorizedUsers = ["admin@test.com"];
+
+  if (authorizedUsers.includes(email)) {
+    alert("Login successful → Redirect to dashboard");
+  } else {
+    document.getElementById("modal").style.display = "flex";
+  }
+}
+
+function closeModal() {
+  document.getElementById("modal").style.display = "none";
+}
+
+function requestAccess() {
+  alert("Access request sent to admin (simulate backend)");
+}
+
+function openCreateAccount() {
+  document.getElementById("createAccountModal").style.display = "flex";
+}
+
+function closeCreateAccount() {
+  document.getElementById("createAccountModal").style.display = "none";
+}
