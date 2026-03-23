21bf2d47edc42a4932625c45e11f8cf0109821af
Riot sam <161737809+Riotsham@users.noreply.github.com>
2026-03-23 11:15:03 +0530

    Delete Dashboard directory

--- Dashboard/script.js
+++
@@ -1,8 +0,0 @@
-let index = 0;
-const slides = document.getElementById("slides");
-const totalSlides = slides.children.length;
-
-setInterval(() => {
-  index = (index + 1) % totalSlides;
-  slides.style.transform = `translateX(-${index * 100}%)`;
-}, 3000);
